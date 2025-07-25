a6debeafb873570be7b1db8ba5bb6f8bd8f0e820
Kostya <kostantinkorocenko301@gmail.com>
2025-07-26 01:36:52 +0300

    test

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,9 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  return (
+    <div className="block" data-cy="TabContent">
+      {activeTab.content}
+    </div>
+  );
+};

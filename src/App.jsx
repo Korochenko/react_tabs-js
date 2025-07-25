262cdd88b9e4510443c09b4f75882e39bb10e301
Kostya <kostantinkorocenko301@gmail.com>
2025-07-26 01:46:51 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -23,48 +23,7 @@ export const App = () => {
       <h1 className="title">Selected tab is {activeTab.title}</h1>
 
       <div data-cy="TabsComponent">
-        <div className="tabs is-boxed">
-          <ul>
-            <li
-              className={activeTabId === 'tab-1' ? 'is-active' : ''}
-              data-cy="Tab"
-            >
-              <a
-                href="#tab-1"
-                data-cy="TabLink"
-                onClick={() => handleTabSelect('tab-1')}
-              >
-                Tab 1
-              </a>
-            </li>
-
-            <li
-              data-cy="Tab"
-              className={activeTabId === 'tab-2' ? 'is-active' : ''}
-            >
-              <a
-                href="#tab-2"
-                data-cy="TabLink"
-                onClick={() => handleTabSelect('tab-2')}
-              >
-                Tab 2
-              </a>
-            </li>
-
-            <li
-              data-cy="Tab"
-              className={activeTabId === 'tab-3' ? 'is-active' : ''}
-            >
-              <a
-                href="#tab-3"
-                data-cy="TabLink"
-                onClick={() => handleTabSelect('tab-3')}
-              >
-                Tab 3
-              </a>
-            </li>
-          </ul>
-        </div>
+        
         <Tabs
           tabs={tabs}
           activeTabId={activeTabId}
